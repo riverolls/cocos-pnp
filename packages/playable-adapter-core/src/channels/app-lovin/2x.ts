1ef222344dd3a92fa93692285d829b24e6e49323
EThan <sun591854735@qq.com>
2023-11-22 16:18:17 +0800

    去掉app loving渠道添加mraid.js的代码

--- packages/playable-adapter-core/src/channels/app-lovin/2x.ts
+++ packages/playable-adapter-core/src/channels/app-lovin/2x.ts
@@ -1,17 +1,13 @@
-import { getChannelRCSdkScript } from '@/utils'
-import { exportSingleFile } from '@/exporter/2x'
-import { AD_SDK_SCRIPT } from './inject-vars'
-import { TChannel, TChannelPkgOptions } from "@/typings"
+import { getChannelRCSdkScript } from "@/utils";
+import { exportSingleFile } from "@/exporter/2x";
+import { AD_SDK_SCRIPT } from "./inject-vars";
+import { TChannel, TChannelPkgOptions } from "@/typings";
 
 export const export2xAppLovin = async (options: TChannelPkgOptions) => {
-  const channel: TChannel = 'AppLovin'
+  const channel: TChannel = "AppLovin";
 
   await exportSingleFile({
     ...options,
     channel,
-    transformHTML: async ($) => {
-      const sdkInjectScript = getChannelRCSdkScript(channel) || AD_SDK_SCRIPT
-      $(sdkInjectScript).appendTo('head')
-    },
-  })
-}
+  });
+};
